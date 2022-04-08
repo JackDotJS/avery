76ad44996dc1375ed7a22416c010d3071705637e
Kyle Edwards <wingedasterisk@gmail.com>
2022-04-07 22:09:05 -0700

    twitter feed test + config (finally, actual code)

---
+++ index.ts
@@ -0,0 +1,31 @@
+import { ETwitterStreamEvent, TweetStream, TwitterApi, ETwitterApiError } from 'twitter-api-v2';
+import cfg from './config/config.json' assert { type: "json" };
+
+const tClient = new TwitterApi(cfg.twitter.bearer_token);
+
+const tweetRules = await tClient.v2.updateStreamRules({
+  add: [
+    { value: cfg.twitter.params }
+  ]
+});
+
+const tweetStream = await tClient.v2.searchStream();
+tweetStream.autoReconnect = true;
+
+tweetStream.on(ETwitterStreamEvent.ConnectionError, (err) => {
+  console.log(`oopsie lol`, err);
+});
+
+tweetStream.on(ETwitterStreamEvent.ConnectionClosed, () => {
+  console.log(`ok bye stupid`);
+});
+
+tweetStream.on(ETwitterStreamEvent.Data, (data) => {
+  console.log(`new tweet:`, data);
+});
+
+tweetStream.on(ETwitterStreamEvent.DataKeepAlive, () => {
+  console.log(`keep alive`);
+});
+
+console.log(`ready`);
