092464162ce18cfbe85a4d4ff730b15f0d7651ef
Kyle Edwards <wingedasterisk@gmail.com>
2022-04-09 17:19:58 -0700

    process: add rly basic discord bot stuff

--- src/process.ts
+++ src/process.ts
@@ -1,5 +1,67 @@
 import { Logger } from './util/logger.js';
+import keys from '../config/keys.json' assert { type: "json" };
+import { Client, Intents, ClientOptions } from 'discord.js';
 
 const log = new Logger();
 
-log.info(`helloooOOOOOOOO`);
+const djsOpts: ClientOptions = {
+  presence: {
+    status: `online`
+  },
+  allowedMentions: { parse: [`users`, `roles`] }, // remove this line to die instantly
+  intents: [
+    // https://discord.com/developers/docs/topics/gateway#list-of-intents
+    Intents.FLAGS.GUILD_MESSAGES
+  ],
+  partials: [
+    `CHANNEL`
+  ]
+};
+
+const bot = new Client(djsOpts);
+
+bot.on(`ready`, (client) => {
+  log.info([
+    `=== Discord API Connection Established! ===`,
+    `Successfully connected with ${client.guilds.cache.size} guild(s)`
+  ].join(`\n`));
+});
+
+bot.on(`guildUnavailable`, (guild) => {
+  log.warn([
+    `=== Guild Unavailable! ===`,
+    `Unable to connect to "${guild.name}" (${guild.id})`,
+  ].join(`\n`));
+});
+
+bot.on(`guildUpdate`, (oldGuild, newGuild) => {
+  if (oldGuild.available && newGuild.available) return;
+
+  log.warn([
+    `=== Guild Now Available! ===`,
+    `"${newGuild.name}" (${newGuild.id}) has recovered.`,
+  ].join(`\n`));
+});
+
+bot.on(`debug`, (message) => {
+  log.debug(message);
+});
+
+bot.on(`warn`, (message) => {
+  log.warn(message);
+});
+
+bot.on(`error`, (error) => {
+  log.error(error.stack || error);
+});
+
+bot.login(keys.discord).catch((error) => {
+  log.fatal(error.stack || error);
+
+  log.warn(`Failed to connect to Discord API. Restarting in 5 minutes...`);
+  // 5 minute timeout to give the API some time to be restored.
+  // this is assuming the issue is on Discord's end.
+  setTimeout(() => {
+    process.exit(1);
+  }, (1000 * 60 * 5));
+});
