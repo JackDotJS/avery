d954fb05a72d2404c8f18214c473c65b57ba2391
Kyle Edwards <wingedasterisk@gmail.com>
2022-04-09 21:09:21 -0700

    twitter: more event handlers, discord integration

--- src/modules/twitter.ts
+++ src/modules/twitter.ts
@@ -1,10 +1,20 @@
 import { ETwitterStreamEvent, TweetStream, TwitterApi, ETwitterApiError } from 'twitter-api-v2';
 import cfg from '../../config/config.json' assert { type: "json" };
 import keys from '../../config/keys.json' assert { type: "json" };
+import { Logger } from '../util/logger.js';
+import { bot } from '../process.js';
+import { inspect } from 'util';
+
+const log = new Logger();
+
+const guild = await bot.guilds.fetch({ guild: cfg.twitter.output.guild });
+const channel = await guild.channels.fetch(cfg.twitter.output.channel);
+
+// todo: queue tweets if discord is down
 
 const tClient = new TwitterApi(keys.twitter);
 
-const tweetRules = await tClient.v2.updateStreamRules({
+await tClient.v2.updateStreamRules({
   add: [
     { value: cfg.twitter.params }
   ]
@@ -14,19 +24,68 @@ const tweetStream = await tClient.v2.searchStream();
 tweetStream.autoReconnect = true;
 
 tweetStream.on(ETwitterStreamEvent.ConnectionError, (err) => {
-  console.log(`oopsie lol`, err);
+  log.error([
+    `=== Twitter API Error ===`,
+    `Error occurred connecting to Twitter API:`,
+    err.stack || err
+  ].join(`\n`));
 });
 
 tweetStream.on(ETwitterStreamEvent.ConnectionClosed, () => {
-  console.log(`ok bye stupid`);
+  log.warn([
+    `=== Twitter API Error ===`,
+    `Twitter API connection closed.`
+  ].join(`\n`));
 });
 
-tweetStream.on(ETwitterStreamEvent.Data, (data) => {
-  console.log(`new tweet:`, data);
+tweetStream.on(ETwitterStreamEvent.Reconnected, () => {
+  log.info([
+    `=== Twitter API Error ===`,
+    `Connection re-established with Twitter API.`
+  ].join(`\n`));
+});
+
+tweetStream.on(ETwitterStreamEvent.DataError, (err) => {
+  log.error([
+    `=== Twitter API Error ===`,
+    `Received error payload from Twitter API:`,
+    err.stack || err
+  ].join(`\n`));
+});
+
+tweetStream.on(ETwitterStreamEvent.TweetParseError, (err) => {
+  log.error([
+    `=== Twitter API Error ===`,
+    `Error occurred while parsing data:`,
+    err.stack || err
+  ].join(`\n`));
+});
+
+tweetStream.on(ETwitterStreamEvent.ReconnectError, (count) => {
+  log.error([
+    `=== Twitter API Error ===`,
+    `Error occurred while reconnecting to Twitter API.`,
+  ].join(`\n`));
+});
+
+tweetStream.on(ETwitterStreamEvent.ReconnectLimitExceeded, () => {
+  log.error([
+    `=== Twitter API Error ===`,
+    `Could not reconnect to Twitter API. (exceeded retry limit)`,
+  ].join(`\n`));
 });
 
 tweetStream.on(ETwitterStreamEvent.DataKeepAlive, () => {
-  console.log(`keep alive`);
+  log.debug(`Twitter API KeepAlive`);
 });
 
-console.log(`ready`);
+tweetStream.on(ETwitterStreamEvent.Data, async (data) => {
+  log.info([
+    `=== New Tweet Received ===`,
+    inspect(data)
+  ].join(`\n`));
+
+  if (channel != null && channel.type === `GUILD_TEXT` && guild.available) {
+    channel.send(`https://twitter.com/JackDotJS/status/${data.data.id}`)
+  }
+});
