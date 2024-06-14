c036682b0a7ecb7bfe61df886fc5622aac90819d
JackDotJS <jackdotjs@proton.me>
2024-06-14 08:33:42 -0700

    add /about command

---
+++ src/modules/discord/slash_commands/about.ts
@@ -0,0 +1,22 @@
+import { ChatInputCommandInteraction, SlashCommandBuilder, EmbedBuilder } from "discord.js";
+import pkg from "../../../../package.json";
+
+export const metadata = new SlashCommandBuilder()
+  .setName(`about`)
+  .setDescription(`Gives some basic information about FeatherBot.`);
+
+export async function response(interaction: ChatInputCommandInteraction) {
+  const embed = new EmbedBuilder()
+    .setColor(`#F34848`)
+    .setTitle(`FeatherBot ${pkg.version}`)
+    .setDescription(`The best bot in the entire world.`)
+    .addFields({
+      name: `Source Code on GitHub`,
+      value: `https://github.com/JackDotJS/featherbot`
+    });
+
+  await interaction.reply({
+    embeds: [ embed ],
+    ephemeral: true
+  });
+}
