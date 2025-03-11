b832862841936284917c8d534d43f22e1a14dd8e
JackDotJS <jackdotjs@proton.me>
2025-03-11 00:56:48 -0700

    command loader: check for name/alias conflicts
    Signed-off-by: JackDotJS <jackdotjs@proton.me>

--- src/modules/discord/commandLoader.ts
+++ src/modules/discord/commandLoader.ts
@@ -14,30 +14,56 @@ export async function initializeCommands() {
     throw new Error(`Could not find command directory: ${cmdDir}`);
   }
 
-  for (const item of readdirSync(cmdDir, { withFileTypes: true })) {
+  mainLoop: for (const item of readdirSync(cmdDir, { withFileTypes: true })) {
     if (item.isDirectory()) continue;
 
     const module = await import(`${cmdDir}/${item.name}`);
-    const command = module.default;
+    const newCmd = module.default;
 
-    if ((command instanceof BaseCommand) == false) {
-      log.debug(command instanceof BaseCommand, typeof command, command);
+    if ((newCmd instanceof BaseCommand) == false) {
+      log.debug(newCmd instanceof BaseCommand, typeof newCmd, newCmd);
       log.warn(`Failed to load command "${item.name}" (Not a valid command)`);
       continue;
     }
 
-    if (command.metadata == null) {
-      log.debug(command);
+    if (newCmd.metadata == null) {
+      log.debug(newCmd);
       log.warn(`Failed to load command "${item.name}" (Missing metadata)`);
       continue;
     }
 
-    if (command.discordHandler == null) {
+    for (const existingCmd of memory.commands) {
+      if (existingCmd.metadata.name == newCmd.metadata.name) {
+        log.warn(`Failed to load command "${item.name}" (Name already exists)`);
+        continue mainLoop;
+      }
+
+      if (existingCmd.metadata.aliases && existingCmd.metadata.aliases.includes(newCmd.metadata.name)) {
+        log.warn(`Failed to load command "${item.name}" (Name conflicts with alias of "${existingCmd.metadata.name}")`);
+        continue mainLoop;
+      }
+
+      if (newCmd.metadata.aliases) {
+        for (const alias of newCmd.metadata.aliases) {
+          if (existingCmd.metadata.name == alias) {
+            log.warn(`Failed to load command "${item.name}" (Alias "${alias}" conflicts with name of "${existingCmd.metadata.name}")`);
+            continue mainLoop;
+          }
+
+          if (existingCmd.metadata.aliases && existingCmd.metadata.aliases.includes(alias)) {
+            log.warn(`Failed to load command "${item.name}" (Alias "${alias}" already in use by "${existingCmd.metadata.name}")`);
+            continue mainLoop;
+          }
+        }
+      }
+    }
+
+    if (newCmd.discordHandler == null) {
       log.info(`Failed to load command "${item.name}" (Not executable in Discord context)`);
       continue;
     }
 
-    memory.commands.push(command);
+    memory.commands.push(newCmd);
     log.info(`Successfully loaded command "${item.name}"`);
   }
 
