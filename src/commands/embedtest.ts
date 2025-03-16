0a4d2f290c3fee325f79e1f2336d29a301457520
JackDotJS <jackdotjs@proton.me>
2025-03-16 06:59:00 -0700

    remove getIcon util
    Signed-off-by: JackDotJS <jackdotjs@proton.me>

--- src/commands/embedtest.ts
+++ src/commands/embedtest.ts
@@ -1,11 +1,11 @@
-import { EmbedBuilder, Message as DiscordMessage, ColorResolvable } from "discord.js";
+import { Message as DiscordMessage } from "discord.js";
 import { Message as RevoltMessage } from "revolt.js";
 // eslint-disable-next-line @typescript-eslint/quotes
 import pkg from "../../package.json" with { type: 'json' };
 // eslint-disable-next-line @typescript-eslint/quotes
 import cfg from '../../config/config.json' with { type: 'json' };
 import { type Command, type CommandMetadata } from "../types/Command.js";
-import { getIconDiscord } from "../util/getIcon.js";
+import { UniversalEmbed } from "../util/universalEmbed.js";
 
 const metadata: CommandMetadata = {
   name: `embedtest`,
@@ -14,18 +14,9 @@ const metadata: CommandMetadata = {
 };
 
 async function discordHandler(message: DiscordMessage) {
-  const attachment1 = await getIconDiscord(`info.png`, `default`);
-  const attachment2 = await getIconDiscord(`checkmark.png`, `success`);
-  const attachment3 = await getIconDiscord(`warning.png`, `warning`);
-  const attachment4 = await getIconDiscord(`error.png`, `error`);
-
-  const embed1 = new EmbedBuilder()
-    .setAuthor({
-      iconURL: `attachment://${attachment1.name}`,
-      name: `embed 1`
-    })
-    .setColor(cfg.colors.default as ColorResolvable)
-    .setTitle(`default`)
+  const embed1 = await new UniversalEmbed(message)
+    .setIcon(`info.png`)
+    .setTitle(`embed 1`)
     .setDescription([
       `# header 1`,
       `## header 2`,
@@ -48,35 +39,40 @@ async function discordHandler(message: DiscordMessage) {
       `\`inline code block\``,
       ``,
       `|| spoilered text ||`
-    ].join(`\n`));
-  
-  const embed2 = new EmbedBuilder()
-    .setAuthor({
-      iconURL: `attachment://${attachment2.name}`,
-      name: `embed 2`
-    })
-    .setColor(cfg.colors.success as ColorResolvable)
-    .setTitle(`success`);
+    ].join(`\n`))
+    .generateObjects();
 
-  const embed3 = new EmbedBuilder()
-    .setAuthor({
-      iconURL: `attachment://${attachment3.name}`,
-      name: `embed 3`
-    })
-    .setColor(cfg.colors.warning as ColorResolvable)
-    .setTitle(`warning`);
+  const embed2 = await new UniversalEmbed(message)
+    .setVibe(`success`)
+    .setIcon(`checkmark.png`)
+    .setTitle(`embed 2`)
+    .generateObjects();
 
-  const embed4 = new EmbedBuilder()
-    .setAuthor({
-      iconURL: `attachment://${attachment4.name}`,
-      name: `embed 4`
-    })
-    .setColor(cfg.colors.error as ColorResolvable)
-    .setTitle(`error`);
+  const embed3 = await new UniversalEmbed(message)
+    .setVibe(`warning`)
+    .setIcon(`warning.png`)
+    .setTitle(`embed 3`)
+    .generateObjects();
+
+  const embed4 = await new UniversalEmbed(message)
+    .setVibe(`error`)
+    .setIcon(`error.png`)
+    .setTitle(`embed 4`)
+    .generateObjects();
 
   await message.reply({
-    embeds: [ embed1, embed2, embed3, embed4 ],
-    files: [ attachment1, attachment2, attachment3, attachment4 ]
+    embeds: [ 
+      embed1.embed, 
+      embed2.embed, 
+      embed3.embed, 
+      embed4.embed 
+    ],
+    files: [ 
+      ...embed1.attachments,
+      ...embed2.attachments,
+      ...embed3.attachments,
+      ...embed4.attachments
+    ]
   });
 }
 
